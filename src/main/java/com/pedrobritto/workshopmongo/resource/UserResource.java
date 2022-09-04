c0306cfeb261a34918d17d8cee1e8637baa1abf4
Pedro Britto <pedro7britto@gmail.com>
2022-09-04 18:57:33 -0300

    Inserção de usuário com POST

--- src/main/java/com/pedrobritto/workshopmongo/resource/UserResource.java
+++ src/main/java/com/pedrobritto/workshopmongo/resource/UserResource.java
@@ -4,13 +4,11 @@ import com.pedrobritto.workshopmongo.domain.User;
 import com.pedrobritto.workshopmongo.dto.UserDTO;
 import com.pedrobritto.workshopmongo.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -34,5 +32,13 @@ public class UserResource {
     public ResponseEntity<UserDTO> findById(@PathVariable String id) {
         User user = service.findById(id);
         return ResponseEntity.ok().body(new UserDTO(user));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+        User obj = service.fromDTO(objDto);
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
--- src/main/java/com/pedrobritto/workshopmongo/service/UserService.java
+++ src/main/java/com/pedrobritto/workshopmongo/service/UserService.java
@@ -1,6 +1,7 @@
 package com.pedrobritto.workshopmongo.service;
 
 import com.pedrobritto.workshopmongo.domain.User;
+import com.pedrobritto.workshopmongo.dto.UserDTO;
 import com.pedrobritto.workshopmongo.repository.UserRepository;
 import com.pedrobritto.workshopmongo.service.exception.ObjectNotFoundException;
 import org.springframework.stereotype.Service;
@@ -24,5 +25,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return repository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
